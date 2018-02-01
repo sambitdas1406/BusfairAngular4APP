578546d96127a172d7e2d3829a18ad8e312b9e56
USER <USER@192.168.0.8>
2018-02-01 17:29:55 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {HttpClientModule} from '@angular/common/http';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCardModule, MatDatepickerModule, MatInputModule, MatNativeDateModule, MatRadioModule,
+   MatToolbarModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
+import { SearchCardComponent } from './search-card.component';
+import { SearchResultComponent } from './search-result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchCardComponent,
+    SearchResultComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+   MatButtonModule,
+    MatCardModule,
+    MatDatepickerModule,
+   MatInputModule,
+    MatNativeDateModule,
+    MatRadioModule,
+   MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
